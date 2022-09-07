9a8b44f7a8d0301a0cbe38ab65475de8305cebb5
sandergol <sandergol@outlook.com>
2022-09-07 12:00:32 +0400

    Write functions "Joke.get_joke/0" and "Joke.print_joke/0"

--- lib/joke.ex
+++ lib/joke.ex
@@ -15,4 +15,27 @@ defmodule Joke do
   def hello do
     :world
   end
+
+  @spec get_joke :: %{
+          :__struct__ => HTTPoison.AsyncResponse | HTTPoison.Response,
+          optional(:body) => String.t,
+          optional(:headers) => list,
+          optional(:id) => reference,
+          optional(:request) => HTTPoison.Request.t(),
+          optional(:request_url) => String.t,
+          optional(:status_code) => integer
+        }
+  def get_joke do
+    HTTPoison.get! "https://api.chucknorris.io/jokes/random"
+  end
+
+  @spec print_joke :: Jason | String.t
+  def print_joke do
+    response_joke = get_joke()
+    if response_joke.status_code == 200 do
+      response_joke.body |> Jason.decode()
+    else
+      "Получен некорректный ответ сервера: #{response_joke.status_code}"
+    end
+  end
 end
